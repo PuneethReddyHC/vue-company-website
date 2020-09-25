044f8d8a869b970254f87816fc2c501f7c4dc50d
puneethreddyhc <puneethreddy951@gmail.com>
2020-09-25 22:12:07 +0530

    Initial dist subtree commit

---
+++ precache-manifest.ad2654db6db3c837b537f291e4c75d9f.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = [
+  {
+    "revision": "4deac41c26aa71afb1a0",
+    "url": "/my-site/css/app.0168a467.css"
+  },
+  {
+    "revision": "4deac41c26aa71afb1a0",
+    "url": "/my-site/js/app.5b6ccbb1.js"
+  },
+  {
+    "revision": "628ffb8cee2a710c8826",
+    "url": "/my-site/js/chunk-vendors.9f90145e.js"
+  },
+  {
+    "revision": "bac5f3b20e87f142ed12c7f1c7e85d9e",
+    "url": "/my-site/img/logo2_footer.bac5f3b2.png"
+  },
+  {
+    "revision": "3527ae94e0b7def90d9b1458bb14eada",
+    "url": "/my-site/img/App2.3527ae94.png"
+  },
+  {
+    "revision": "ebf2f3f4fa6920c7ea512bd4c2cca145",
+    "url": "/my-site/img/close.ebf2f3f4.svg"
+  },
+  {
+    "revision": "423a8955f63c328c2139e68befb5b03a",
+    "url": "/my-site/img/logo.423a8955.png"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/my-site/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/my-site/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/my-site/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/my-site/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "46abbc4a676739dbd61f8a305cb63fd8",
+    "url": "/my-site/img/nucleo-icons.46abbc4a.svg"
+  },
+  {
+    "revision": "840ba33eca823c31f87738e65a726304",
+    "url": "/my-site/img/say-shape-left.840ba33e.png"
+  },
+  {
+    "revision": "4d3b5a34630d7bf3fb11d0e82b62c252",
+    "url": "/my-site/img/App1.4d3b5a34.png"
+  },
+  {
+    "revision": "a8aed15764dabe9d5b8fcee86c452681",
+    "url": "/my-site/img/best-features.a8aed157.png"
+  },
+  {
+    "revision": "df5bea5932e195b10582b731f21f1d37",
+    "url": "/my-site/img/app_btn1.df5bea59.png"
+  },
+  {
+    "revision": "8dea359037ea32af0ebcd5723fd223a0",
+    "url": "/my-site/img/dog.8dea3590.png"
+  },
+  {
+    "revision": "59d74250cbef3612f82aacba83855001",
+    "url": "/my-site/img/App3.59d74250.png"
+  },
+  {
+    "revision": "6feb3259236801455d4269f273a22e98",
+    "url": "/my-site/img/say-shape-right.6feb3259.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/my-site/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/my-site/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/my-site/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/my-site/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/my-site/img/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "d9459441a436e8a1180f31e49da637c0",
+    "url": "/my-site/img/app_btn2.d9459441.png"
+  },
+  {
+    "revision": "868f52830126d7e0367b5a524ec274bd",
+    "url": "/my-site/img/hero_right.868f5283.png"
+  },
+  {
+    "revision": "c9736ac2771b9f0f16dddd395b23c76a",
+    "url": "/my-site/index.html"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/my-site/favicon.png"
+  },
+  {
+    "revision": "a06a3525da854143cc38f8df7e73d44e",
+    "url": "/my-site/animate.min.css"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/my-site/img/brand/blue.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/my-site/img/brand/favicon.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/my-site/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/my-site/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/my-site/img/brand/white.png"
+  },
+  {
+    "revision": "4d3b5a34630d7bf3fb11d0e82b62c252",
+    "url": "/my-site/img/gallery/App1.png"
+  },
+  {
+    "revision": "1de1914ab9df4ec3b93b9f582c56e540",
+    "url": "/my-site/img/gallery/1.png"
+  },
+  {
+    "revision": "e73e316d1c8eed00c3da29fc7d98ffbe",
+    "url": "/my-site/img/gallery/2.png"
+  },
+  {
+    "revision": "1fe7112622a0dd60161e508a335a29bc",
+    "url": "/my-site/img/gallery/3.png"
+  },
+  {
+    "revision": "3527ae94e0b7def90d9b1458bb14eada",
+    "url": "/my-site/img/gallery/App2.png"
+  },
+  {
+    "revision": "59d74250cbef3612f82aacba83855001",
+    "url": "/my-site/img/gallery/App3.png"
+  },
+  {
+    "revision": "39acd0671163a606fdde812f06524055",
+    "url": "/my-site/img/gallery/card_dot.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/my-site/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/my-site/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/my-site/img/theme/landing.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/my-site/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "5b0fd72bc806b44ead570ed1a6579f54",
+    "url": "/my-site/img/gallery/best_pricingbg.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/my-site/img/theme/profile.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/my-site/img/theme/promo-1.png"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/my-site/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/my-site/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/my-site/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/my-site/img/theme/team-4-800x800.jpg"
+  }
+];
